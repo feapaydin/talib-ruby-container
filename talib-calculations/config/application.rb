41095a093b5cf4d19fc90522543155b482472315
Furkan Enes <feapaydin@gmail.com>
2021-12-20 11:11:54 +0300

    heroku host added

--- talib-calculations/config/application.rb
+++ talib-calculations/config/application.rb
@@ -36,5 +36,7 @@ module TalibCalculations
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << 'stormy-bastion-53241.herokuapp.com'
   end
 end
